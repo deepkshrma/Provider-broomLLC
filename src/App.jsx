375b62d81f05ded844a8c0c12761137887c4e4f6
deepak <sharmads2001@gmail.com>
2025-08-12 16:52:59 +0530

    add toast

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import "./App.css";
+import { ToastContainer } from "react-toastify";
 
 // Pages
 import Login from "./Pages/Sign/Login";
@@ -27,174 +28,176 @@ import UpdateProfile from "./Pages/Profile/UpdateProfile";
 import Layout from "./Layout";
 import ProtectedRoute from "./routes/ProtectedRoute";
 
-
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        {/* Public route */}
-        <Route path="/Login" element={<Login />} />
+    <>
+    <ToastContainer position="top-right" autoClose={3000} />
+      <BrowserRouter>
+        <Routes>
+          {/* Public route */}
+          <Route path="/Login" element={<Login />} />
 
-        {/* Redirect root to dashboard if token exists */}
-        <Route path="/" element={<Navigate to="/Dashboard" replace />} />
+          {/* Redirect root to dashboard if token exists */}
+          <Route path="/" element={<Navigate to="/Dashboard" replace />} />
 
-        {/* Protected routes within Layout */}
-        <Route path="/" element={<Layout />}>
-          <Route
-            path="Dashboard"
-            element={
-              <ProtectedRoute>
-                <Dashboard />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="Profile"
-            element={
-              <ProtectedRoute>
-                <Profile />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="UpdateProfile"
-            element={
-              <ProtectedRoute>
-                <UpdateProfile />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="ChangePassword"
-            element={
-              <ProtectedRoute>
-                <ChangePassword />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="BookingRequest"
-            element={
-              <ProtectedRoute>
-                <BookingRequest />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="BookingRequest1"
-            element={
-              <ProtectedRoute>
-                <BookingRequest1 />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="BookingRequest2"
-            element={
-              <ProtectedRoute>
-                <BookingRequest2 />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="BookingRequest3"
-            element={
-              <ProtectedRoute>
-                <BookingRequest3 />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="BookingRequest4"
-            element={
-              <ProtectedRoute>
-                <BookingRequest4 />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="Messages"
-            element={
-              <ProtectedRoute>
-                <Messages />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="AllServices"
-            element={
-              <ProtectedRoute>
-                <AllServices />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="AllServices1"
-            element={
-              <ProtectedRoute>
-                <AllServices1 />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="MyServices"
-            element={
-              <ProtectedRoute>
-                <MyServices />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="MySubscription"
-            element={
-              <ProtectedRoute>
-                <MySubscription />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="ServiceRequestList"
-            element={
-              <ProtectedRoute>
-                <ServiceRequestList />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="ServiceRequestList1"
-            element={
-              <ProtectedRoute>
-                <ServiceRequestList1 />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="ServicemanList"
-            element={
-              <ProtectedRoute>
-                <ServicemanList />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="RoleUpdate"
-            element={
-              <ProtectedRoute>
-                <RoleUpdate />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="AddNewServiceman"
-            element={
-              <ProtectedRoute>
-                <AddNewServiceman />
-              </ProtectedRoute>
-            }
-          />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+          {/* Protected routes within Layout */}
+          <Route path="/" element={<Layout />}>
+            <Route
+              path="Dashboard"
+              element={
+                <ProtectedRoute>
+                  <Dashboard />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="Profile"
+              element={
+                <ProtectedRoute>
+                  <Profile />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="UpdateProfile"
+              element={
+                <ProtectedRoute>
+                  <UpdateProfile />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="ChangePassword"
+              element={
+                <ProtectedRoute>
+                  <ChangePassword />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="BookingRequest"
+              element={
+                <ProtectedRoute>
+                  <BookingRequest />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="BookingRequest1"
+              element={
+                <ProtectedRoute>
+                  <BookingRequest1 />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="BookingRequest2"
+              element={
+                <ProtectedRoute>
+                  <BookingRequest2 />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="BookingRequest3"
+              element={
+                <ProtectedRoute>
+                  <BookingRequest3 />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="BookingRequest4"
+              element={
+                <ProtectedRoute>
+                  <BookingRequest4 />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="Messages"
+              element={
+                <ProtectedRoute>
+                  <Messages />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="AllServices"
+              element={
+                <ProtectedRoute>
+                  <AllServices />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="AllServices1"
+              element={
+                <ProtectedRoute>
+                  <AllServices1 />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="MyServices"
+              element={
+                <ProtectedRoute>
+                  <MyServices />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="MySubscription"
+              element={
+                <ProtectedRoute>
+                  <MySubscription />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="ServiceRequestList"
+              element={
+                <ProtectedRoute>
+                  <ServiceRequestList />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="ServiceRequestList1"
+              element={
+                <ProtectedRoute>
+                  <ServiceRequestList1 />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="ServicemanList"
+              element={
+                <ProtectedRoute>
+                  <ServicemanList />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="RoleUpdate"
+              element={
+                <ProtectedRoute>
+                  <RoleUpdate />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="AddNewServiceman"
+              element={
+                <ProtectedRoute>
+                  <AddNewServiceman />
+                </ProtectedRoute>
+              }
+            />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
